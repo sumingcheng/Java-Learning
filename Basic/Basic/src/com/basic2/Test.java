1924dea6f908b7730163632c3fda6e0c145bcd21
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-22 22:18:25 +0800

    javaDoc

---
+++ Basic/Basic/src/com/basic2/Test.java
@@ -0,0 +1,15 @@
+package com.basic2;
+/*
+ *  javaDoc 可以直接在命令行使用，生成文档
+ *  javadoc -d doc -encoding utf-8 .\Test.java
+ */
+
+/**
+ * 这是一个Test类，会帮助生成帮助文档的文本
+ */
+public class Test {
+    public static void main(String[] args) {
+        //java变量
+
+    }
+}
