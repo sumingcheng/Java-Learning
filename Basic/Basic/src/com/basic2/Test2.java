2c356fcd6b65ecc78183d8c0c6d5e687470b69fd
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-22 22:41:42 +0800

    变量复习

---
+++ Basic/Basic/src/com/basic2/Test2.java
@@ -0,0 +1,15 @@
+package com.basic2;
+
+/*
+ *  变量是什么？
+ *  1.是内存中存储数据最基本的单元，是特定的一块存储空间
+ * */
+public class Test2 {
+    public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 2;
+
+        System.out.println(a + b);
+    }
+}
