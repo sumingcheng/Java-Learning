bd56f43777e8c9714ee171c7eab4429cb7eef4c4
Sumingcheng <fanxiaohaohaha@qq..com>
2023-03-27 23:01:16 +0800

    数据类型

---
+++ Basic/Basic/src/com/basic3/Test2.java
@@ -0,0 +1,24 @@
+package com.basic3;
+
+public class Test2 {
+    public static void main(String[] args) {
+        /*
+         * java中的基本类型
+         * 整数类型：byte,short,int,long(8)
+         * 浮点类型：float,double(8)
+         * 字符类型：char(2)
+         * 布尔类型：boolean(4)
+         * */
+
+        /*
+         * 类型转换方式
+         * 自动类型转换：低位向高位转换
+         * 强制类型转换：高位向低位转换
+         * 布尔类型不能转换为其他类型
+         * */
+
+//        范围不同的类型不能直接赋值
+//        byte b = 100;
+//        char i = b;
+    }
+}
