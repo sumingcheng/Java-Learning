58335a368904e040e4d81285f1445a9ec3e1c438
Sumingcheng <fanxiaohaohaha@qq..com>
2023-04-17 21:11:22 +0800

    final

---
+++ src/com/practice_basic1_6/Calendar.java
@@ -0,0 +1,23 @@
+package com.practice_basic1_6;
+
+/*
+ * 1900 年 1 月 1 日 是 星期一
+ * */
+
+import java.util.Scanner;
+
+public class Calendar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int initYear = 1900;
+        int totalDaysFrom = 0;
+
+        System.out.println("请输入年份：");
+        int year = scanner.nextInt();
+
+        System.out.println("请输入月份：");
+        int mount = scanner.nextInt();
+
+    }
+}
