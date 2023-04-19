d173921284123668bc129995566246bf278676e5
Sumingcheng <fanxiaohaohaha@qq..com>
2023-04-19 21:22:12 +0800

    方法

---
+++ src/com/basic7/Test2.java
@@ -0,0 +1,27 @@
+package com.basic7;
+
+import java.util.Scanner;
+
+/*
+ * 计算器
+ * */
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void Calculator() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("请输入第一个数：");
+        int num1 = sc.nextInt();
+
+        System.out.println("请输入第二个数：");
+        int num2 = sc.nextInt();
+        System.out.println("请输入运算类型");
+        String type = sc.next();
+        int result = 0;
+
+
+    }
+}
