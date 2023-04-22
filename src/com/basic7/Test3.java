c57683106c454d87fc0bb7c16493710511d11a13
Sumingcheng <fanxiaohaohaha@qq..com>
2023-04-22 20:22:30 +0800

    方法的重载，说白了就是很多个方法等待一个符合条件的参数进来

---
+++ src/com/basic7/Test3.java
@@ -0,0 +1,22 @@
+package com.basic7;
+
+/*
+ * 方法的参数
+ * */
+public class Test3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int test(int a, int b) {
+        return a + b;
+    }
+
+    public static float test(float a, float b) {
+        return a + b;
+    }
+
+    public static long test(long a, long b) {
+        return a + b;
+    }
+}
