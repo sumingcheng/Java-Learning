72e350e8e36d50583de49ad41a2fb0a3cfe26766
Sumingcheng <fanxiaohaohaha@qq..com>
2023-04-24 22:12:29 +0800

    Arrays

---
+++ src/com/basic8/Test.java
@@ -0,0 +1,28 @@
+package com.basic8;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+/*
+ * 数组
+ * */
+public class Test {
+    public static void main(String[] args) {
+//        String[] arr = new String[5];
+//        System.out.println(arr.length);
+//
+//        for (int i = 0; i < 5; i++) {
+//            arr[i] = String.valueOf(i);
+//            System.out.println(arr[i]);
+//        }
+
+        /*
+         * com.basic8.Test [I@1b6d3586
+         * [ => 几个 [ 就代表几维数组
+         * */
+
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
