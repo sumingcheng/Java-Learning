52b0109c741736a4dfe45cabc1aa1b25720fdc2c
Sumingcheng <fanxiaohaohaha@qq..com>
2023-05-05 21:34:32 +0800

    bubbling

--- src/com/arraylgorthm/BubbleSort.java
+++ src/com/arraylgorthm/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.arraylgorthm;
 
+import java.util.Arrays;
+
 /*
  * 冒泡排序
  * 核心，大数向右，小数向左
@@ -9,5 +11,29 @@ public class BubbleSort {
         // 数组排序
         int[] arr = {3, 5, 1, 0, 6};
 
+        int[] bubblingArr = bubbling(arr);
+
+        System.out.println(Arrays.toString(bubblingArr));
+    }
+
+    public static int[] bubbling (int[] arr) {
+        int[] _arr = arr;
+        int len = arr.length;
+        int temp;
+
+        for (int j = len - 1; j > 0; j--) {
+
+            for (int i = 0; i < j; i++) {
+                if (_arr[i] > _arr[i + 1]) {
+                    temp = _arr[i];
+                    _arr[i] = _arr[i + 1];
+                    _arr[i + 1] = temp;
+                } else {
+
+                }
+            }
+
+        }
+        return _arr;
     }
 }
