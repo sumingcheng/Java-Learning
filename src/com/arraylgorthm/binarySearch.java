9b94af5ff809f3252f4921936d1702fd1d8bdca6
Sumingcheng <fanxiaohaohaha@qq..com>
2023-05-15 21:23:05 +0800

    BinarySearch

--- src/com/arraylgorthm/binarySearch.java
+++ src/com/arraylgorthm/binarySearch.java
@@ -2,8 +2,27 @@ package com.arraylgorthm;
 
 public class binarySearch {
     public static void main (String[] args) {
-        int[] arr = {2, 5, 7, 4, 7, 1, 0, 2};
-
-
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+        int index = BinarySearch(arr, target);
+        System.out.println("Index of " + target + " is: " + index);
     }
+
+    public static int BinarySearch (int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
