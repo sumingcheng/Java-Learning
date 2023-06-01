07002d4477e2903e00f1c26bb698abcc8034b1f6
Sumingcheng <fanxiaohaohaha@qq..com>
2023-06-01 21:51:23 +0800

    oop

--- src/com/todoList/Test.java
+++ src/com/todoList/Test.java
@@ -5,6 +5,7 @@ package com.todoList;
  * */
 public class Test {
     public static void main (String[] args) {
-
+        TodoListClass todoListClass = new TodoListClass("吃饭", false);
+        System.out.println(todoListClass.getTodoList());
     }
 }
