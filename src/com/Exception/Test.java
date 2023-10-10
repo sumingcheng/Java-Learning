e9c28e83edcc80549685e971ad36f9f6ec120968
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-10 18:00:29 +0800

    fix：异常

---
+++ src/com/Exception/Test.java
@@ -0,0 +1,7 @@
+package com.Exception;
+
+public class Test {
+    public static void main (String[] args) {
+
+    }
+}
--- src/com/data_calendar_learning/CalendarMeth.java
+++ src/com/data_calendar_learning/CalendarMeth.java
@@ -4,22 +4,23 @@ import java.util.Calendar;
 
 public class CalendarMeth {
     public static void main (String[] args) {
-        Calendar cal = Calendar.getInstance();
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
 
-//        cal.set(Calendar.DAY_OF_MONTH, 1);
-//        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
-//        cal.set(Calendar.DAY_OF_MONTH, 15);
-//        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+        cal1.set(Calendar.DAY_OF_MONTH, 1);
+        System.out.println(cal1.get(Calendar.DAY_OF_MONTH));
+        cal2.set(Calendar.DAY_OF_MONTH, 15);
+        System.out.println(cal2.get(Calendar.DAY_OF_MONTH));
 
-        test(cal);
-        test1(cal);
+//        test(cal);
+//        test1(cal);
     }
     // 只要操作的是同一个实例，获取的都是更改以后的
-    public static void test (Calendar cal) {
-        cal.add(Calendar.MONTH, 1);
-    }
-
-    public static void test1 (Calendar cal) {
-        System.out.println(cal.get(Calendar.MONTH));
-    }
+//    public static void test (Calendar cal) {
+//        cal.add(Calendar.MONTH, 1);
+//    }
+//
+//    public static void test1 (Calendar cal) {
+//        System.out.println(cal.get(Calendar.MONTH));
+//    }
 }
