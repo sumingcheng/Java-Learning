360364575ecc7058c3e1990998501dee9cde66d3
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-10 10:31:18 +0800

    fix：下限通配符

--- src/com/Generics/family/Test.java
+++ src/com/Generics/family/Test.java
@@ -9,12 +9,18 @@ public class Test {
         printName(familyGrandpa);
         Family<Father> familyFather = new Family<>(father);
         printName(familyFather);
-        Family<Son> familySon = new Family<>(son);
-        printName(familySon);
+//        Family<Son> familySon = new Family<>(son);
+//        printName(familySon);
     }
 
-    public static void printName (Family<? extends Grandpa> family) {
-        Grandpa person = family.getPerson();
-        System.out.println(person.getName());
+    // 上限
+//    public static void printName (Family<? extends Grandpa> family) {
+//        Grandpa person = family.getPerson();
+//        System.out.println(person.getName());
+//    }
+    // 下限
+    public static void printName (Family<? super Father> family) {
+        Object person = family.getPerson();
+//        System.out.println(person.getName());
     }
 }
