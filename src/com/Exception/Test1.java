26f4011144ec6317a6a44facb80766c1c4468674
sumingcheng <cjwdyzdyplxjg@gmail.com>
2023-10-11 14:00:47 +0800

    fix：异常处理

---
+++ src/com/Exception/Test1.java
@@ -0,0 +1,30 @@
+package com.Exception;
+
+public class Test1 {
+    public static void main (String[] args) {
+//        try {
+//            int num = Integer.parseInt("XYZ");
+//        } catch (NumberFormatException e) {
+//            e.printStackTrace();
+//        }
+
+//        int[] arr = new int[5];
+//        try {
+//            int x = arr[10];
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            FileInputStream file = new FileInputStream("non_existing_file.txt");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            FileInputStream file = new FileInputStream("non_existing_file.txt");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
